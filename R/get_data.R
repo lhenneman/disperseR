4eb02d249d079494275be2a24938289c6183fc1a
garbulinskamaja <majagarbulinska@hsph.harvard.edu>
2019-09-03 16:00:34 +0200

    get data now donwload everything

--- R/get_data.R
+++ R/get_data.R
@@ -2,6 +2,7 @@
 ##########################################################################################
 
 ## This function is used below in get_data().
+
 download_file <- function(url, file, dir) {
   out <- tryCatch({
     download.file(url = url, destfile = file)
@@ -28,6 +29,7 @@ download_file <- function(url, file, dir) {
       return(NULL)
     },
     finally = {
+
     })
   return(out)
 }
@@ -40,18 +42,159 @@ get_data <-
     start.year = NULL,
     start.month = NULL,
     end.year = NULL,
-    end.month = NULL,
-    unit = NULL,
-    linked.zips.data = NULL,
-    linked.zips.data.position = NULL) {
-
+    end.month = NULL) {
     # to avoid error of unused arguments we used them independently of data requested
     startyear <- start.year
     startmonth <- start.month
     endyear <- end.year
     endmonth <- end.month
-    linked_zips_data <- linked.zips.data
-    linked_zips_data_position <- linked.zips.data.position
+
+    if (data == "all") {
+
+      ### Crosswalk
+      message("Reading in the crosswalk data from disperseR")
+      crosswalk <- disperseR::crosswalk
+      assign("crosswalk", crosswalk, envir = .GlobalEnv)
+      message("   Assigned to crosswalk variable")
+
+      ### PP.units.monthly1995_2017
+      message("Reading in the PP.units.monthly1995_2017 data from disperseR")
+      PP.units.monthly1995_2017 <- disperseR::PP.units.monthly1995_2017
+      assign("PP.units.monthly1995_2017", PP.units.monthly1995_2017, envir = .GlobalEnv)
+      message("   Assigned to PP.units.monthly1995_2017 variable")
+
+      ### PP.units.monthly1995_2017
+
+      message("Reading in the zipcodecoordinate data from disperseR")
+      zipcodecoordinate <- disperseR::zipcodecoordinate
+      assign("zipcodecoordinate", zipcodecoordinate, envir = .GlobalEnv)
+      message("   Assigned to zipcodecoordinate variable")
+
+      ### ALL DATA
+      ### ZCTA shape files
+      message("Start getting the ZCTA file")
+
+      directory <- zcta_dir
+      file <- file.path(directory, 'cb_2017_us_zcta510_500k.zip')
+      url <-
+        'ftp://ftp2.census.gov/geo/tiger/GENZ2017/shp/cb_2017_us_zcta510_500k.zip'
+
+      if (!file.exists(file)) {
+        message(
+          "   Downloading the ZCTA shapefile.
+          This might take a bit, but sometimes the url will fail to connect, in this case you might want to try again later"
+        )
+        download_file(url, file, directory)
+      }
+      else{
+        message("   File already exist, not downloading.")
+      }
+
+      message("   Starting Preprocessing")
+      zcta <- file.path(directory, 'cb_2017_us_zcta510_500k.shp')
+      zcta <- raster::shapefile(x = zcta)
+      # It is recommended to transform the ZCTA shapefile to a known projection to maintain consistency throughout the allocation process.
+      # Lat-lon projections are preferred, such as the [North American Albers Equal Area Conic](https://epsg.io/102008):
+      p4s <-
+        "+proj=aea +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m"
+      zcta.trans <- sp::spTransform(x = zcta, CRSobj = p4s)
+      message("   Preprocessing complete")
+      assign("zcta", zcta.trans, envir = .GlobalEnv)
+      message("   Assigned to zcta variable")
+
+      ### ZCTA dataset
+
+      message("Start getting the ZCTA dataset for graphs")
+
+      message("   Starting Preprocessing")
+      zcta <- file.path(directory, 'cb_2017_us_zcta510_500k.shp')
+      zcta <- sf::st_read(zcta)
+      data.table::setnames(zcta, 'ZCTA5CE10', 'ZCTA')
+      zcta <-
+        merge(
+          zcta,
+          disperseR::crosswalk,
+          by = "ZCTA",
+          all = F,
+          allow.cartesian = TRUE
+        )
+      message("   Preprocessing complete")
+      assign("zcta_dataset", zcta, envir = .GlobalEnv)
+      message("   Assigned to zcta_dataset variable")
+
+      ### pblheight
+
+      ## planetary layers data
+      message("Start getting the planetary layers data")
+      directory <- hpbl_dir
+      file <- file.path(directory, 'hpbl.mon.mean.nc')
+      url <-
+        'https://www.esrl.noaa.gov/psd/repository/entry/get/hpbl.mon.mean.nc?entryid=synth%3Ae570c8f9-ec09-4e89-93b4-babd5651e7a9%3AL05BUlIvTW9udGhsaWVzL21vbm9sZXZlbC9ocGJsLm1vbi5tZWFuLm5j'
+
+      if (!file.exists(file)) {
+        message(
+          "   Downloading Planetary Layers Data.
+          This might take a bit, but sometimes the url will fail to connect, in this case you might want to try again later"
+        )
+        download_file(url, file, directory)
+      } else{
+        message("   File already exist, not downloading.")
+      }
+
+      #Before reading in, it is necessary to set the system time zone to UTC so that the dates are formatted correctly in the raster files.
+      Sys.setenv(TZ = 'UTC')
+      hpbl_rasterin <-
+        suppressWarnings(raster::brick(x = file, varname = 'hpbl'))
+      # The following is done to fix error in the dataset. For more information please see
+      # https://stackoverflow.com/questions/56806894/raster-warning-message-in-cbindmi-vals-number-of-rows-of-result-is-not/56807318#56807318
+      raster::crs(hpbl_rasterin) <-
+        "+proj=lcc +x_0=5632642.22547 +y_0=4612545.65137 +lat_0=50 +lon_0=-107 +lat_1=50"
+      message("   Preprocessing complete")
+      assign("pblheight", hpbl_rasterin, envir = .GlobalEnv)
+      message("   Assigned to  pblheight variable")
+
+      ###  metfiles
+
+      message("Start getting the metfiles data")
+      if (is.null(startyear) |
+          is.null(startmonth) |
+          is.null(endyear) | is.null(endmonth)) {
+        stop("Please specify the metfiles dates correctly. Please refer to documentation")
+
+        inputdates <-
+          c(
+            paste(startyear, startmonth, "01", sep = "/"),
+            paste(endyear, endmonth, "01", sep = "/")
+          )
+        start <- as.Date(inputdates[1])
+        end <- as.Date(inputdates[2])
+
+        vectorfiles <- NULL
+        i = 1
+        while (start <= end) {
+          ## process strings for download information
+          string <-
+            paste("RP", format(start, "%Y%m"), ".gbl", sep = "")
+          vectorfiles[i] <- string
+          lubridate::month(start) <- lubridate::month(start) + 1
+          i = i + 1
+        }
+        metfiles <-
+          vectorfiles[!(vectorfiles %in% list.files(meteo_dir))]
+
+        if (length(metfiles) > 0) {
+          message("   Downloading the following files:")
+          message(metfiles)
+          disperseR::get_met_reanalysis(files = metfiles, path_met_files = meteo_dir)
+        }
+        if (length(metfiles) == 0) {
+          message("   No files to download. Requested files already available")
+        }
+      }
+    }
+
+
+
 
     if (data == "zctashapefile" | data == "zcta_dataset") {
       directory <- zcta_dir
@@ -71,7 +214,8 @@ get_data <-
     # download meteo files
     if (data == "metfiles") {
       if (is.null(startyear) |
-          is.null(startmonth) | is.null(endyear) | is.null(endmonth)) {
+          is.null(startmonth) |
+          is.null(endyear) | is.null(endmonth)) {
         stop("Please specify the metfiles dates correctly. Please refer to documentation")
       }
       inputdates <-
@@ -86,7 +230,8 @@ get_data <-
       i = 1
       while (start <= end) {
         ## process strings for download information
-        string <- paste("RP", format(start, "%Y%m"), ".gbl", sep = "")
+        string <-
+          paste("RP", format(start, "%Y%m"), ".gbl", sep = "")
         vectorfiles[i] <- string
         lubridate::month(start) <- lubridate::month(start) + 1
         i = i + 1
@@ -104,7 +249,7 @@ get_data <-
       }
     }
 
-    if (data != "metfiles") {
+    if (data != "metfiles" & data != "all") {
       if (!file.exists(file)) {
         message(
           "This might take a bit, but sometimes the url will fail to connect, in this case you might want to try again later"
@@ -123,7 +268,7 @@ get_data <-
       zcta <-
         merge(
           zcta,
-          hyspdisp2::crosswalk,
+          disperseR::crosswalk,
           by = "ZCTA",
           all = F,
           allow.cartesian = TRUE
@@ -137,7 +282,8 @@ get_data <-
       zcta <- raster::shapefile(x = zcta)
       # It is recommended to transform the ZCTA shapefile to a known projection to maintain consistency throughout the allocation process.
       # Lat-lon projections are preferred, such as the [North American Albers Equal Area Conic](https://epsg.io/102008):
-      p4s <- "+proj=aea +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m"
+      p4s <-
+        "+proj=aea +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m"
       zcta.trans <- sp::spTransform(x = zcta, CRSobj = p4s)
       message("Preprocessing complete")
       return(zcta.trans)
@@ -150,9 +296,10 @@ get_data <-
         suppressWarnings(raster::brick(x = file, varname = 'hpbl'))
       # The following is done to fix error in the dataset. For more information please see
       # https://stackoverflow.com/questions/56806894/raster-warning-message-in-cbindmi-vals-number-of-rows-of-result-is-not/56807318#56807318
-      raster::crs(hpbl_rasterin) <- "+proj=lcc +x_0=5632642.22547 +y_0=4612545.65137 +lat_0=50 +lon_0=-107 +lat_1=50"
+      raster::crs(hpbl_rasterin) <-
+        "+proj=lcc +x_0=5632642.22547 +y_0=4612545.65137 +lat_0=50 +lon_0=-107 +lat_1=50"
       message("Preprocessing complete")
       return(hpbl_rasterin)
     }
-  }
+    }
 
