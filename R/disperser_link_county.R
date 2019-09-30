9e0514392c54cdbdeaaed388d41252c3261c6ee4
Lucas <henneman@hsph.harvard.edu>
2019-09-30 12:55:29 -0400

    aesthetics

--- R/disperser_link_county.R
+++ R/disperser_link_county.R
@@ -66,8 +66,7 @@ disperser_county_link <- function( month_YYYYMM = NULL,
     print(  paste( Sys.time(), "Files read and combined"))
 
     ## Trim dates & first hour
-    d <- d[d$Pdate %in% as( c( vec_dates), "character") &
-             hour > 1,]
+    d <- d[d$Pdate %in% as( c( vec_dates), "character") & hour > 1,]
 
     #Check if extent matches the hpbl raster
     d_xmin <- min( d$lon)
