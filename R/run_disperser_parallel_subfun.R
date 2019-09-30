df315cc60bf49c118a9d8b13a8639054689d130e
Lucas <henneman@hsph.harvard.edu>
2019-09-30 12:56:41 -0400

    link_zip now handles counties, grids

--- R/run_disperser_parallel_subfun.R
+++ R/run_disperser_parallel_subfun.R
@@ -591,113 +591,116 @@ subset_nc_date <- function(hpbl_file = NULL,
 ################# link_zip
 #' @export link_zip
 link_zip <- function(d,
-  zc = zcta2,
-  cw = crosswalk,
-  gridfirst = F,
-  rasterin = NULL) {
-  xy <- d[, .(lon, lat)]
-  spdf.in <- SpatialPointsDataFrame(
-    coords = xy,
-    data = d,
-    proj4string = CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
-  )
-  spdf <- spTransform(spdf.in,
-    proj4string(zc))
+                     p4string,
+                     zc = NULL,
+                     cw = NULL,
+                     county.sp = NULL,
+                     rasterin = NULL,
+                     return.grid = F) {
 
-  if (gridfirst == F) {
-    o <- over(spdf, zc)
-    D <- data.table(na.omit(cbind(d, o)))
-  } else {
-    # extract data layer from raster, disaggregate to .1x.1
-    if (is.null(rasterin) == T)
-      stop("Need PBL raster!")
+    xy <- d[, .(lon, lat)]
+    spdf.in <- SpatialPointsDataFrame( coords = xy,
+                                       data = d,
+                                       proj4string = CRS( "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
+    spdf <- spTransform( spdf.in, p4string)
+
+    # extract data layer from raster, disaggregate to .1°x.1°
     pbl_layer <- subset_nc_date(hpbl_brick = rasterin,
-      vardate = d$Pdate[1])
-
-    #   suppressWarnings(
-    pbl_layer.t <- projectRaster(pbl_layer,
-      crs = CRS(proj4string(spdf)))
-    #   )
+                                vardate = d$Pdate[1])
+    pbl_layer.t <- projectRaster( pbl_layer,
+                                  crs = CRS( proj4string( spdf)))
 
     # aim for a resolution of 12 km
-    pbl_resolution <- res(pbl_layer.t)
-    x_fact <- floor(pbl_resolution[1] / 12000)
-    y_fact <- floor(pbl_resolution[2] / 12000)
-    pbl_layer.d <- disaggregate(pbl_layer.t,
-      fact = c(x_fact, y_fact))
+    pbl_resolution <- res( pbl_layer.t)
+    x_fact <- floor( pbl_resolution[1] / 12000)
+    y_fact <- floor( pbl_resolution[2] / 12000)
+    pbl_layer.d <- disaggregate( pbl_layer.t,
+                                 fact = c( x_fact, y_fact))
 
     # count number of particles in each cell,
     # find original raster cells, divide number by pbl
     r <- pbl_layer.d
-    values(r) <- NA
-    cells <- cellFromXY(r, spdf)
-    tab <- table(cells)
-    pbls <- pbl_layer.d[as.numeric(names(tab))]
-    r[as.numeric(names(tab))] <- tab / pbls
+    values( r) <- NA
+    cells <- cellFromXY( r, spdf)
+    tab <- table( cells)
+    pbls <- pbl_layer.d[as.numeric( names( tab))]
+    r[as.numeric( names( tab))] <- tab / pbls
 
     # crop around point locations for faster extracting
-    # and convert to polygons for faster extracting
     e <- extent(spdf)
-    r2 <- crop(trim(r,
-      padding = 1),
-      e)
+    r2 <- crop( trim(r,
+                     padding = 1),
+                e)
+
+    # if return.grid, return xyz object
+    if( return.grid){
+      xyz <- data.table( rasterToPoints(r2))
+      names(xyz)[3] <- 'hyspdisp'
+
+      return( xyz)
+    }
+
+    #  convert to polygons for faster extracting
     r3 <- rasterToPolygons(r2)
 
+    # if county.so, return xyz object
+    if( !is.null( county.sp)){
+      print( 'Linking counties!')
+      county.o <- over( county.sp,
+                        r3,
+                        fn = mean)
+      return( county.o)
+    }
+
     #crop zip codes to only use ones over the extent
-    zc_trim <- crop(zc,
-      snap = 'out',
-      e)
+    zc_trim <- crop( zc,
+                     snap = 'out',
+                     e)
 
     zc_groups <- ceiling(seq_along(zc_trim) / 1000)
 
     #extract average concentrations over zip codes
     #name column as 'N', combine with zip codes
     #define function to not run out of memory
-    over_fn <- function(group,
-      zc_dt,
-      groups,
-      raster_obj) {
-      dt <- data.table(over(zc_dt[groups %in% group, ],
-        raster_obj,
-        fn = mean))
+    over_fn <- function( group,
+                         zc_dt,
+                         groups,
+                         raster_obj) {
+
+      dt <- data.table( over( zc_dt[ groups %in% group,],
+                              raster_obj,
+                              fn = mean))
 
       # if "over" returned no matches, need a vector of NA's
-      if (nrow(dt) == 1 & is.na(dt[1])) {
-        dt <-
-          data.table(X = as.numeric(rep(NA, length(zc_dt[groups %in% group, ]))))
-        setnames(dt, "X", names(raster_obj))
+      if( nrow( dt) == 1 & is.na( dt[1])){
+        dt <- data.table( X = as.numeric( rep( NA, length( zc_dt[ groups %in% group,]))))
+        setnames( dt, "X", names( raster_obj))
       }
-      return(dt)
+
+      return( dt)
     }
 
-    or <- rbindlist(lapply(
-      unique(zc_groups),
-      over_fn,
-      zc_dt = zc_trim,
-      groups = zc_groups,
-      raster_obj = r3
-    ))
+    or <- rbindlist( lapply( unique( zc_groups),
+                             over_fn,
+                             zc_dt = zc_trim,
+                             groups = zc_groups,
+                             raster_obj = r3))
 
     setnames(or, names(pbl_layer), 'N')
-    D <- data.table(cbind(zc_trim@data,
-      or))
-  }
-  setnames(D, 'ZCTA5CE10', 'ZCTA')
-  cw$ZCTA <- formatC(cw$ZCTA,
-    width = 5,
-    format = "d",
-    flag = "0") # to merge on zcta ID
-  M <-
-    merge(D,
-      cw,
-      by = "ZCTA",
-      all = F,
-      allow.cartesian = TRUE) # all.x = TRUE, all.y = FALSE, allow.cartesian = TRUE)
-  M[, ZIP := formatC(ZIP,
-    width = 5,
-    format = "d",
-    flag = "0")]
-  M$ZIP <- as(M$ZIP, 'character')
-  M <- na.omit(M)
-  return(M)
+    D <- data.table( cbind( zc_trim@data,
+                            or))
+
+    setnames( D, 'ZCTA5CE10', 'ZCTA')
+    cw$ZCTA <- formatC( cw$ZCTA,
+                        width = 5,
+                        format = "d",
+                        flag = "0") # to merge on zcta ID
+    M <- merge( D, cw, by = "ZCTA", all = F, allow.cartesian = TRUE) # all.x = TRUE, all.y = FALSE, allow.cartesian = TRUE)
+    M[, ZIP:= formatC( ZIP,
+                       width = 5,
+                       format = "d",
+                       flag = "0")]
+    M$ZIP <- as(M$ZIP, 'character')
+    M <- na.omit( M)
+    return(M)
 }
