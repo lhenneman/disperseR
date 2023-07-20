84740464cbd6243691a29938378febe4eebbd2dc
Michael Ash <michaelaoash@gmail.com>
2023-07-20 16:17:14 -0400

    Trying to fix problem with unit variable

--- R/link_all_units.R
+++ R/link_all_units.R
@@ -145,7 +145,9 @@ link_all_units<- function(units.run,
   if( link.to == 'grids')
     out <- units.run[, grids_link_parallel(.SD), by = seq_len(nrow(units.run))]
 
-  out[, comb := paste("month: ", out[, .(month)], " unitID :", out[, .(unit)], sep = "")]
+  print(out)
+    
+  out[, comb := paste("month: ", out[, .(month)], " unitID : ", out[, .(unit)], sep = "")]
   out[, seq_len := NULL]
   return(out)
 }
